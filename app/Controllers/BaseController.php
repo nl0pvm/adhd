bf4b1b28189e3a751aad6109523cdb2848be6142
Peter V <nl0pvm@users.noreply.github.com>
2025-07-18 10:01:54 +0200

    Enable CSRF protection and initialize session

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -7,6 +7,7 @@ use CodeIgniter\HTTP\CLIRequest;
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Session\Session;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -41,7 +42,12 @@ abstract class BaseController extends Controller
      * Be sure to declare properties for any property fetch you initialized.
      * The creation of dynamic property is deprecated in PHP 8.2.
      */
-    // protected $session;
+    /**
+     * Session instance
+     *
+     * @var Session
+     */
+    protected $session;
 
     /**
      * @return void
@@ -53,6 +59,6 @@ abstract class BaseController extends Controller
 
         // Preload any models, libraries, etc, here.
 
-        // E.g.: $this->session = service('session');
+        $this->session = service('session');
     }
 }
